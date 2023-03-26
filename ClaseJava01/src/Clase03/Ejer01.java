dda9bcd342e8d4ded0bf0eed282787458bf65eaf
gshoji <gastonshoji@gmail.com>
2023-03-26 11:05:31 -0300

    Update Ejer01.java

--- ClaseJava01/src/Clase03/Ejer01.java
+++ ClaseJava01/src/Clase03/Ejer01.java
@@ -5,17 +5,7 @@ public class Ejer01 {
 
 	public static void main(String[] args) {
 		
-		String palabra="araxi";
-		String letra='a';
-		int cont=0;
-		for(int i=0;i<palabra.length();i++)
-		{
-			if(palabra.contains(letra))
-			{
-				cont++;
-			}
-		}
-		System.out.println("La palabra "+palabra+" contiene "+cont+" letras "+let);
+		System.out.println("<"+"_"+"-"+"@"+"| / // "+"\\  \\\\#¿¡¿!{}[]()<>");
 		
 	}
 
